7a915904fe4113afcd146f163a7e690f83cc87ce
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-08-01 19:38:41 -0400

    DOM traversal downside

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const h1 = document.getElementById('main-title');
+// const h1 = document.getElementById('main-title');
 
-console.log('Before ', h1.textContent);
+// console.log('Before ', h1.textContent);
 
-h1.textContent = 'Some new Title';
-h1.style.color = "white";
-h1.style.backgroundColor = "green";
+// h1.textContent = 'Some new Title';
+// h1.style.color = "white";
+// h1.style.backgroundColor = "green";
 
-console.log('After ', h1.textContent);
+// console.log('After ', h1.textContent);
 
-console.dir(h1);
+// console.dir(h1);
+
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
+
+console.log(firstLi); //breaks the DOM Traversal technique
+
